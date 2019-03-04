af2eedc102409dbe535222c5e5e7f6186d4ad979
Vijay Yadav <vijay.1.yadav@niit-tech.com>
2019-03-04 19:20:28 +0530

    highlighted weekday
    highlighted weekday

--- FSCalendar/FSCalendar.m
+++ FSCalendar/FSCalendar.m
@@ -1173,12 +1173,12 @@ typedef NS_ENUM(NSUInteger, FSCalendarOrientation) {
                 NSCalendar *calendar = [NSCalendar currentCalendar];
                 NSDateComponents *dateComponent = [calendar components:(NSCalendarUnitWeekOfMonth) fromDate: firstDate];
                 NSLog(@"%@",dateComponent);
-                height = 37 * (int)dateComponent.weekOfMonth;
+                height = 37 * (int)dateComponent.weekOfMonth+40;
             }else{
                 NSCalendar *calendar = [NSCalendar currentCalendar];
                 NSDateComponents *dateComponent = [calendar components:(NSCalendarUnitWeekOfMonth) fromDate: [NSDate date]];
                 NSLog(@"%@",dateComponent);
-                height = 37 * (int)dateComponent.weekOfMonth;
+                height = 37 * (int)dateComponent.weekOfMonth+40;
             }
             _isFreshInitialization = false;
         }
